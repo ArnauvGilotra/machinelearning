cd98e34dae5ee9952ff483d75a45ddcf444cd726
ArnauvGilotra <arnauvgilotra@gmail.com>
2022-06-06 18:01:46 +0530

    Understanding Logistic Regression

---
+++ LogisticRegression.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+
+
+# import data
+cancer = datasets.load_breast_cancer()
+X_cancer = cancer['data']
+y_cancer = cancer['target']
+wine = datasets.load_wine()
+X_wine = wine['data']
+y_wine = wine['target']
+
+binary_model = LogisticRegression(C = 10**5, max_iter = 1e5)
+binary_model.fit(X_cancer, y_cancer)
+
+
+y_hats = binary_model.predict(X_cancer)
+p_hats = binary_model.predict_proba(X_cancer)
+print(f'Training accuracy: {binary_model.score(X_cancer, y_cancer)}')
